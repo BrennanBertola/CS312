dce96527898e63e6447c83264e5976363e77fb0a
BrennanBertola <brennan.bertola@comcast.net>
2022-09-08 19:32:38 -0600

    code for proj1 done, report needed

--- Project1/fermat.py
+++ Project1/fermat.py
@@ -5,37 +5,52 @@ def prime_test(N, k):
 	# This is main function, that is connected to the Test button. You don't need to touch it.
 	return fermat(N,k), miller_rabin(N,k)
 
-
-def mod_exp(x, y, N):
-    # You will need to implement this function and change the return value.   
-	return 1
+# all significant time complexities are noted next to
+# function and are further explained in the report
+def mod_exp(x, y, N): # O(log(y))
+    if y == 0: return 1
+    z = mod_exp(x, y//2, N)
+    if y % 2: return x*z**2%N #if y is odd
+    else: return z**2%N #if y is even
 	
 
 def fprobability(k):
-    # You will need to implement this function and change the return value.   
-    return 0.0
+    return 1 - (1/2)**k #we test k times with each time having a 1/2 chance of failing
 
 
 def mprobability(k):
-    # You will need to implement this function and change the return value.   
-    return 0.0
+    return 1 - (1/4)**k #we test k times with each time having a 1/4 chance of failing
+
+def fermat(N,k): #O(k*log(N))
+    #checks for base cases
+    if N == 1: return 'composite'
+    if N == 2 or N == 3: return 'prime'
+
+    for i in range(k): # will check with k different values for a
+        a = random.randint(2, N - 1)
+        if mod_exp(a, N-1, N) != 1: return 'composite'
+
+    return 'prime'
+
+def miller_rabin(N,k):# O(k*log(N)^2)
+    #checks for base cases
+    if N == 1: return 'composite'
+    if N == 2 or N == 3: return 'prime'
+
+    for i in range(k): # will check with k different values for a
+        y = N-1
+        a = random.randint(2, N - 1)
+        while(1): # contiues to halve y until y is odd, a -1 is returned, or a non 1/-1 is returned
+            result = mod_exp(a, y, N)
+            if result == N - 1: return 'prime'
+            if result != 1: return 'composite'
+            if y % 2 == 1: break
+            y = y//2
+
+    return 'prime'
 
 
-def fermat(N,k):
-    # You will need to implement this function and change the return value, which should be
-    # either 'prime' or 'composite'.
-	#
-    # To generate random values for a, you will most likley want to use
-    # random.randint(low,hi) which gives a random integer between low and
-    #  hi, inclusive.
-	return 'prime'
-
-
-def miller_rabin(N,k):
-    # You will need to implement this function and change the return value, which should be
-    # either 'prime' or 'composite'.
-	#
-    # To generate random values for a, you will most likley want to use
-    # random.randint(low,hi) which gives a random integer between low and
-    #  hi, inclusive.
-	return 'composite'
+# used for testing/debugging
+if __name__ == '__main__':
+    result = miller_rabin(97, 1)
+    print (result)
